270a67515198c23d07c8a588f4f7e831886d14ec
Denis Ametov <dametov@in.nurtelecom.kg>
2025-07-09 11:08:19 +0600

    updated LDAP auth

--- src/main/java/com/nurtel/duty_schedule/security/SecurityConfig.java
+++ src/main/java/com/nurtel/duty_schedule/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.Customizer;
@@ -16,7 +17,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.ldap.authentication.BindAuthenticator;
+import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
 import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
+import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Arrays;
@@ -28,9 +32,12 @@ public class SecurityConfig {
     String ldapUrl;
     @Value("${ldap.domain}")
     String ldapDomain;
-
-    @Autowired
-    private UserAuthService userAuthService;
+    @Value("${ldap.user}")
+    String ldapUser;
+    @Value("${ldap.password}")
+    String ldapPassword;
+    @Value("${ldap.base}")
+    String ldapBase;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -42,8 +49,6 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(BaseRoutes.NOT_SECURED + "/**").permitAll()
-                        .requestMatchers(BaseRoutes.ROOT + "/**").authenticated()
                         .anyRequest().permitAll()
                 )
                 .httpBasic(Customizer.withDefaults())
@@ -51,19 +56,37 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                         .maximumSessions(1)
                 )
-                .userDetailsService(userAuthService)
+                //.userDetailsService(userAuthService)
                 .build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager()
-    {
-        ActiveDirectoryLdapAuthenticationProvider adProvider = new ActiveDirectoryLdapAuthenticationProvider(ldapDomain, ldapUrl);
-        adProvider.setConvertSubErrorCodesToExceptions(true);
-        adProvider.setUseAuthenticationRequestCredentials(true);
-        adProvider.setAuthoritiesMapper(getAuthorityMapper());
-        return new ProviderManager(Arrays.asList(adProvider));
+    public LdapContextSource contextSource() {
+        LdapContextSource source = new LdapContextSource();
+        source.setUrl(ldapUrl);
+        source.setBase(ldapBase);
+
+        source.setUserDn(ldapUser);
+        source.setPassword(ldapPassword);
+
+        source.afterPropertiesSet();
+        return source;
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        BindAuthenticator bindAuthenticator = new BindAuthenticator(contextSource());
+        // Ищем пользователя по sAMAccountName
+        bindAuthenticator.setUserSearch(new FilterBasedLdapUserSearch(
+                "", // поиск будет по базовому DN contextSource()
+                "(sAMAccountName={0})",
+                contextSource()
+        ));
+
+        LdapAuthenticationProvider provider = new LdapAuthenticationProvider(bindAuthenticator);
+        return new ProviderManager(provider);
+    }
+
 
     private static SimpleAuthorityMapper getAuthorityMapper()
     {
